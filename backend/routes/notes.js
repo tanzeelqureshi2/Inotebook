939b00772afa8f9dcec6e855dee417943b619aa9
tanzeelqureshi2 <tanzeelqureshi420@gmail.com>
2022-02-08 16:15:46 +0530

    Create Update routes for update a existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -48,4 +48,30 @@ router.post(
   }
 );
 
+//ROUTE 3:Update an existing Note using: POST "/api/notes/updatenote".Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  try {
+    const { title, description, tag } = req.body;
+    //Create a newNote object
+    const newNote={};
+    if(title){newNote.title=title};
+    if(description){newNote.description=description};
+    if(tag){newNote.tag=tag};
+
+    //Find the note to be updated and update it
+    let note=await Note.findById(req.params.id);
+    if(!note){
+     return req.status(401).send("Not Found");
+    }
+    if(note.user.toString()!==req.user.id){
+     return req.status(401).send("Not Allowed");
+    }
+    note=await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+    res.json({note});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Interval Server Error");
+  }
+});
+
 module.exports = router;
