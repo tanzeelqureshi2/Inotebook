5840e6c71b5e0988622bd9cbbff6279ed58d8970
tanzeelqureshi2 <tanzeelqureshi420@gmail.com>
2022-02-08 12:21:33 +0530

    Create new routes for fetch all notes and Add a new note

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'user'
+    },
       title:{
           type:String,
           required:true
@@ -15,8 +20,8 @@ const NotesSchema = new Schema({
     },
     date:{
         type:Date,
-        Default:Date.now
+        default:Date.now
     },
   });
 
-  module.exports=mongoose.model('notes',NotesSchema);
+  module.exports=mongoose.model('note',NotesSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,51 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const Note = require("../models/Note");
+const fetchuser = require("../middleware/fetchuser");
 
-router.get('/',(req,res)=>{
-    
-    res.json([])
-})
+//ROUTE 1:Get All the Notes using: GET "/api/notes/fetchallnotes".Login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Interval Server Error");
+  }
+});
 
-module.exports=router;
+//ROUTE 2:Add a new Note using: POST "/api/notes/addnote".Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Interval Server Error");
+    }
+  }
+);
+
+module.exports = router;
