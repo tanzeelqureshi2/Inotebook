4b485b7f9e0c89557b29eef1115c96aa4820d1bf
tanzeelqureshi2 <tanzeelqureshi420@gmail.com>
2022-02-09 16:26:05 +0530

    Introduction to ContextApi and creating ContextApi for iNotebook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook</h1>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,25 @@
+import NoteContext from "./noteContext";
+import {useState} from "react";
+
+const NoteState=(props)=>{
+      const s1={
+          "name":"Tanzeel",
+          "class":"5b"
+      }
+      const [state, setState] = useState(s1);
+      const update=()=>{
+          setTimeout(() => {
+              setState({
+                "name":"Sharjeel",
+                "class":"4b"   
+              })
+          }, 1000);
+      }
+      return (
+          <NoteContext.Provider value={{state,update}}>
+              {props.children}
+          </NoteContext.Provider>
+      )
+}
+
+export default NoteState;
